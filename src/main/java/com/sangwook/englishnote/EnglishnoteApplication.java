c98b1ac7f224078eb7aded1ab7cc37835ac3cd81
thstkddnr20 <thstkddnr20@naver.com>
2024-08-15 14:41:55 +0900

    initial commit

---
+++ src/main/java/com/sangwook/englishnote/EnglishnoteApplication.java
@@ -0,0 +1,13 @@
+package com.sangwook.englishnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangwook/englishnote/EnglishnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangwook.englishnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
